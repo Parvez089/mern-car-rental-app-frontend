b56f96b88a22f5ff24a90cd873b8ef304a6cdc8d
parvez <mhparvez089@gmail.com>
2025-09-03 10:05:28 +0600

    add detailsPage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,18 @@ import FeaturedSection from "../components/FeaturedSection";
 import Banner from "../components/Banner";
 import Testimonial from "../components/Testimonial";
 import Newsletter from "../components/Newsletter";
-import Footer from "../components/Footer";
+
 
 const Home = () => {
-  return <div>
-    <Hero/>
-    <FeaturedSection/>
-    <Banner/>
-    <Testimonial/>
-    <Newsletter/>
-    <Footer/>
-  </div>;
+  return (
+    <div>
+      <Hero />
+      <FeaturedSection />
+      <Banner />
+      <Testimonial />
+      <Newsletter />
+    </div>
+  );
 };
 
 export default Home;
