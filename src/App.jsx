20543889ec4e99086bda2f300f0c1df0668478ae
parvez <mhparvez089@gmail.com>
2025-09-08 23:06:57 +0600

    owner dashboard fix

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
           <Route index element={<Dashboard />} />
           <Route path='add-car' element={<AddCar />} />
           <Route path='manage-cars' element={<ManageCars />} />
-          <Route path='manage-cars' element={<ManageBookings />} />
+          <Route path='manage-bookings' element={<ManageBookings />} />
         </Route>
       </Routes>
       {!isOwnerPath && <Footer />}
